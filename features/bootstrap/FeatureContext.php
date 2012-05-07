950435807f361a9f641b6535b81e194fcd01643a
everzet <ever.zet@gmail.com>
2012-05-07 17:49:06 +0200

    initial commit

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,14 @@
+<?php
+
+class FeatureContext extends Behat\Behat\Context\BehatContext
+{
+    use Behat\MinkExtension\Context\MinkDictionary;
+
+    /**
+     * @Then /^I wait for the suggestion box to appear$/
+     */
+    public function iWaitForTheSuggestionBoxToAppear()
+    {
+        $this->getSession()->wait(5000, "$('.suggestions-results').children().length > 0");
+    }
+}
